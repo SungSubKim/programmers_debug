d1e4484cb69e70a16117a2b8cbafdc30c35ed047
snow2014 <bang627@skku.edu>
2022-05-14 18:43:18 +0900

    first

---
+++ input_parse.py
@@ -0,0 +1,8 @@
+T = int(input())
+tc,expectation=[],[]
+for _ in range(T):
+    row = input().split('\t')
+    tc.append('['+','.join(row[:-1])+']')
+    expectation.append(row[-1])
+print('testcases = ['+','.join(tc)+']')
+print('expectations = ['+','.join(expectation)+']')
